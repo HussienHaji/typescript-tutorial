bc6bde3b2a965e707f1e1dc5fe4598e82fc29a10
HussienHaji <hussienhaji.dev@gmail.com>
2024-12-16 22:32:20 +0330

    exercising typescript setup

---
+++ 001-hello-world/dist/index..js
@@ -0,0 +1,6 @@
+"use strict";
+function greeting(value) {
+    return "Hello ".concat(value);
+}
+console.log(greeting("World"));
+//# sourceMappingURL=index..js.map
---
+++ 001-hello-world/index..ts
@@ -0,0 +1,5 @@
+function greeting(value: string): string {
+  return `Hello ${value}`;
+}
+
+console.log(greeting("World"));
